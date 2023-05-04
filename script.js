76637f3fe00410e4f5a58761acfb68d21ae0bb64
AJIT <110042310+ajitkumarsah@users.noreply.github.com>
2023-05-04 16:05:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+const quizDB = [
+  {
+    question: "01: Who are you?",
+    a: "Human",
+    b: "Animal",
+    c: "Object",
+    d: "paper",
+    ans: "ans1",
+  },
+  {
+    question: "02: What are you?",
+    a: "Human",
+    b: "Animal",
+    c: "Object",
+    d: "paper",
+    ans: "ans1",
+  },
+  {
+    question: "03: Who are you?",
+    a: "Human",
+    b: "Animal",
+    c: "Object",
+    d: "paper",
+    ans: "ans1",
+  },
+  {
+    question: "04: Who are you?",
+    a: "Human",
+    b: "Animal",
+    c: "Object",
+    d: "paper",
+    ans: "ans1",
+  },
+  {
+    question: "05: Who are you?",
+    a: "Human",
+    b: "Animal",
+    c: "Object",
+    d: "paper",
+    ans: "ans1",
+  },
+  {
+    question: "06: Who are you?",
+    a: "Human",
+    b: "Animal",
+    c: "Object",
+    d: "paper",
+    ans: "ans1",
+  },
+];
+
+const answers = document.querySelectorAll(".answer");
+
+const showscore = document.getElementById("showscore");
+
+const question = document.querySelector(".question");
+const option1 = document.querySelector("#question1");
+const option2 = document.querySelector("#question2");
+const option3 = document.querySelector("#question3");
+const option4 = document.querySelector("#question4");
+const submit = document.querySelector("#submit");
+
+const op1 = document.getElementById("option1");
+const op2 = document.getElementById("option2");
+const op3 = document.getElementById("option3");
+const op4 = document.getElementById("option4");
+
+let questionCount = 0;
+let score = 0;
+
+const loadQuestion = () => {
+  const questionList = quizDB[questionCount];
+  question.innerHTML = questionList.question;
+  op1.innerHTML = questionList.a;
+  op2.innerText = questionList.b;
+  op3.innerText = questionList.c;
+  op4.innerText = questionList.d;
+};
+
+loadQuestion();
+
+const getCheckAnswer = () => {
+  let answer;
+
+  answers.forEach((curAnsElem) => {
+    if (curAnsElem.checked) {
+      answer = curAnsElem.id;
+    }
+  });
+  return answer;
+};
+
+const fetchQns = () => {
+  let answer = getCheckAnswer();
+
+  if (answer == quizDB[questionCount].ans) {
+    score++;
+  }
+
+  questionCount++;
+
+  if (questionCount == quizDB.length) {
+    showscore.innerHTML = `<h1>Hurry! You scored ${score} from ${quizDB.length}</h1>`;
+  }
+
+  loadQuestion();
+};
+
+showscore.innerHTML = `<h1>Your score ${score} / ${quizDB.length}</h1>`;
